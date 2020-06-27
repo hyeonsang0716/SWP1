8ef4dd15bc69b1240be3a5d213b975b4618d689a
hyeonsang0716 <kyr76767@naver.com>
2020-06-27 17:34:35 +0900

    사용자가 입력한 수가 없을 경우 화면에 sum=0 mul=0 출력

--- fig2.py
+++ fig2.py
@@ -5,6 +5,7 @@ def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     x = d.get('x', [''])[0]
     y = d.get('y', [''])[0]
+
     if '' not in [x, y]:
         x, y= int(x), int(y)
     response_body = template2.get_template(x,y)
