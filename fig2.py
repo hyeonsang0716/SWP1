a1ec2f4c36717a867498f881fb56763da94899b0
hyeonsang0716 <kyr76767@naver.com>
2020-06-27 17:42:06 +0900

    issue #2 fig2.py에서 x와 y값을 입력하지 않았을 때 sum과 mul은 0이 출력되도록 변경함.

--- fig2.py
+++ fig2.py
@@ -5,10 +5,12 @@ def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     x = d.get('x', [''])[0]
     y = d.get('y', [''])[0]
-
+    sum, mul = 0, 0
     if '' not in [x, y]:
         x, y= int(x), int(y)
-    response_body = template2.get_template(x,y)
+        sum = x + y
+        mul = x + y
+    response_body = html % {'sum' : sum, 'mul' : mul}
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
         ('Content-Length', str(len(response_body)))
